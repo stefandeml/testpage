f69965266840ac01765f7ba1396a5b60373698a7
sdeml <stefandeml@gmail.com>
2018-12-08 16:11:21 +0100

    das

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -8,6 +8,6 @@ apt-get install -qq libcurl4-openssl-dev libelf-dev libdw-dev binutils-dev libib
 apt-get install -qq python-minimal git 
 python --version
 
-TAG=$(git describe --tags)
-echo "DER TAG"$TAG
+#TAG=$(git describe --tags)
+echo "DER TAG"$CIRCLE_TAG
 
