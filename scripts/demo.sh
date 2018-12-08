278fcae7fa4eddbc0a46f99b77f604e6a9a9976a
sdeml <stefandeml@gmail.com>
2018-12-08 15:42:38 +0100

    das

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -1,8 +1,11 @@
 set -e
-apt-get update
-apt-get -qq install python-minimal git 
+apt-get install -qq curl zlib1g-dev build-essential python
+apt-get install -qq cmake g++ pkg-config jq
+apt-get install -qq libcurl4-openssl-dev libelf-dev libdw-dev binutils-dev libiberty-dev
+
+apt-get install -qq python-minimal git 
 python --version
 
 TAG=$(git describe --tags)
-echo $TAG
+echo "DER TAG"$TAG
 
