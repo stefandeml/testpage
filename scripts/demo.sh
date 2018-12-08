d869f7c82141adbac992e31c8aaa553180f27a95
sdeml <stefandeml@gmail.com>
2018-12-08 15:43:35 +0100

    das

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -1,4 +1,6 @@
 set -e
+
+apt-get update
 apt-get install -qq curl zlib1g-dev build-essential python
 apt-get install -qq cmake g++ pkg-config jq
 apt-get install -qq libcurl4-openssl-dev libelf-dev libdw-dev binutils-dev libiberty-dev
